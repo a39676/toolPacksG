7b66ff81e1f4cdd5d13291a7548decb8f9ad7af8
DavenChan <396767101@qq.com>
2018-07-14 12:28:22 +0800

    新加入http upload file demo

--- src/main/java/httpHandel/HttpUtil.java
+++ src/main/java/httpHandel/HttpUtil.java
@@ -2,10 +2,18 @@ package httpHandel;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -112,17 +120,56 @@ public class HttpUtil {
 	}
 	
 	
-	public static void main(String[] args) throws Exception {
+	public void httpPostUploadFileDemo() throws MalformedURLException, IOException {
 
-		HttpUtil http = new HttpUtil();
-		String host = "http://localhost:8080/";
-		String url = "operation/task/getTaskList.action";
-		// String result = http.sendGet(HOST + url, requestHeaderKeys);
-		String result = http.sendPost(host + url);
-		System.out.println(result);
+		String url = "http://example.com/upload";
+		String charset = "UTF-8";
+		String param = "value";
+		File textFile = new File("/path/to/file.txt");
+		File binaryFile = new File("/path/to/file.bin");
+		String boundary = Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
+		String CRLF = "\r\n"; // Line separator required by multipart/form-data.
 
-		// System.out.println("\nTesting 2 - Send Http POST request");
-		// http.sendPost();
+		URLConnection connection = new URL(url).openConnection();
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+		try (
+		    OutputStream output = connection.getOutputStream();
+		    PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+		) {
+		    // Send normal param.
+		    writer.append("--" + boundary).append(CRLF);
+		    writer.append("Content-Disposition: form-data; name=\"param\"").append(CRLF);
+		    writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+		    writer.append(CRLF).append(param).append(CRLF).flush();
+
+		    // Send text file.
+		    writer.append("--" + boundary).append(CRLF);
+		    writer.append("Content-Disposition: form-data; name=\"textFile\"; filename=\"" + textFile.getName() + "\"").append(CRLF);
+		    writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF); // Text file itself must be saved in this charset!
+		    writer.append(CRLF).flush();
+		    Files.copy(textFile.toPath(), output);
+		    output.flush(); // Important before continuing with writer!
+		    writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+
+		    // Send binary file.
+		    writer.append("--" + boundary).append(CRLF);
+		    writer.append("Content-Disposition: form-data; name=\"binaryFile\"; filename=\"" + binaryFile.getName() + "\"").append(CRLF);
+		    writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(binaryFile.getName())).append(CRLF);
+		    writer.append("Content-Transfer-Encoding: binary").append(CRLF);
+		    writer.append(CRLF).flush();
+		    Files.copy(binaryFile.toPath(), output);
+		    output.flush(); // Important before continuing with writer!
+		    writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+
+		    // End of multipart/form-data.
+		    writer.append("--" + boundary + "--").append(CRLF).flush();
+		}
+
+		// Request is lazily fired whenever you need to obtain information about response.
+		int responseCode = ((HttpURLConnection) connection).getResponseCode();
+		System.out.println(responseCode); // Should be 200
 
 	}
 }

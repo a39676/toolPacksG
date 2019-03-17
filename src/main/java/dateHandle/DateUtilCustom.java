71024c3f15ecc7ea8c6605dce42d62a2cbbe2cfa
DavenChan <396767101@qq.com>
2019-03-17 18:00:57 +0800

    撤销时间格式

--- src/main/java/dateHandle/DateUtilCustom.java
+++ src/main/java/dateHandle/DateUtilCustom.java
@@ -143,8 +143,6 @@ public abstract class DateUtilCustom {
 	    DATE_FORMAT_REGEXPS.put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$", "yyyy/MM/dd HH:mm:ss");
 	    DATE_FORMAT_REGEXPS.put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd MMM yyyy HH:mm:ss");
 	    DATE_FORMAT_REGEXPS.put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd MMMM yyyy HH:mm:ss");
-
-	    DATE_FORMAT_REGEXPS.put("^\\d{4}\\-\\d{1,2}\\-\\d{1,2}t\\d{1,2}:\\d{2}$", "yyyy-MM-ddTHH:mm");
 	}}
 	
 }

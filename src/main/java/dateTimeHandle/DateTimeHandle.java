1a5840cd4a3ba6eedb754cfed1802c56d78435c4
q39676 <396767101@qq.com>
2019-07-29 11:56:21 +0800

    .

--- src/main/java/dateTimeHandle/DateTimeHandle.java
+++ src/main/java/dateTimeHandle/DateTimeHandle.java
@@ -1,32 +1,16 @@
 package dateTimeHandle;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalTime;
 
 public class DateTimeHandle extends DateTimeUtilCommon {
 
-	public static void main(String[] args) {
-		DateTimeHandle d = new DateTimeHandle();
-		d.tmpDateTimeTrans("2018-04-11 18:00", 2);
-
-		String format = DateUtilCustom.determineDateFormat("2018-01-02 12:45:00");
-		System.out.println(format);
-		format = DateUtilCustom.determineDateFormat("2018-01-02T12:45:00");
-		System.out.println(format);
-		format = DateUtilCustom.determineDateFormat("2018-01-02 12:45");
-		System.out.println(format);
+	public static LocalDateTime atStartOfDay(LocalDateTime date) {
+	    return date.with(LocalTime.MIN);
 	}
 
-	public void tmpDateTimeTrans(String dateTimeInput, Integer duration) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(normalDateTimeFormat);
-		LocalDateTime ldt = LocalDateTime.parse(dateTimeInput, formatter);
-		System.out.println(ldt + " , " + ldt.plusHours(duration));
-
-		System.out.println(ldt.plusHours(15) + " , " + ldt.plusHours(15).plusHours(duration));
-
-		LocalDateTime ldt2 = LocalDateTime.parse("2018-12-31T23:59:59");
-		System.out.println(ldt2);
+	public static LocalDateTime atEndOfDay(LocalDateTime date) {
+	    return date.with(LocalTime.MAX);
 	}
 
 }

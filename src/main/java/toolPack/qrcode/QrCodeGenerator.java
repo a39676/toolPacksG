a4058b167ac2a2c3874061c73eead28d58158217
DavenChan <q39676@gmail.com>
2022-01-24 20:29:57 +0800

    Update
    Fix QR code UTF-8

--- src/main/java/toolPack/qrcode/QrCodeGenerator.java
+++ src/main/java/toolPack/qrcode/QrCodeGenerator.java
@@ -3,8 +3,10 @@ package toolPack.qrcode;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Hashtable;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
@@ -15,20 +17,24 @@ public class QrCodeGenerator {
 	private static final int defaultX = 200;
 	private static final int defaultY = 200;
 
+	@SuppressWarnings("unchecked")
 	public BufferedImage generation(String str, Integer x, Integer y) throws WriterException {
 		if (x == null) {
 			x = defaultX;
 		}
-		
+
 		if (y == null) {
 			y = defaultY;
 		}
-		
+
+		@SuppressWarnings("rawtypes")
+		Hashtable hints = new Hashtable();
+		hints.put(EncodeHintType.CHARACTER_SET, StandardCharsets.UTF_8);
+
 		BitMatrix matrix = new MultiFormatWriter().encode(
-				new String((str).getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8),
-				BarcodeFormat.QR_CODE, 
-				x, y);
-		
+				new String((str).getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8), BarcodeFormat.QR_CODE, x, y,
+				hints);
+
 		return MatrixToImageWriter.toBufferedImage(matrix);
 	}
 
